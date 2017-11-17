11efb7ac29cadfe8b2fa751cf189e9c489ca24a2
Javier Ferrer <javier.mailserio@gmail.com>
2017-11-17 19:36:42 +0100

    Case classes visibilities tests

---
+++ src/test/tv/codely/scala_intro_examples/lesson_09_oop/CaseClassVisibilitiesSpec.scala
@@ -0,0 +1,25 @@
+package tv.codely.scala_intro_examples.lesson_09_oop
+
+import org.scalatest.{Matchers, WordSpec}
+
+final class CaseClassVisibilitiesSpec extends WordSpec with Matchers {
+  private val randomText = "some text"
+  private val caseClass  = CaseClassVisibilities(attributeInConstruct = randomText)
+
+  "Case Class" should {
+    "generate a public getter for the attributes defined in the constructor" in {
+      caseClass.attributeInConstruct shouldBe randomText
+    }
+    "not compile if we try to access private attributes defined in the constructor" in {
+      "caseClass.privateAttributeInConstruct" shouldNot compile
+    }
+    "set as public the attributes defined in the class body by default" in {
+      val bodyAttributeValue = "public body attribute value"
+
+      caseClass.attributeInBody shouldBe bodyAttributeValue
+    }
+    "not compile if we try to access private attributes defined in the body" in {
+      "caseClass.privateAttributeInBody" shouldNot compile
+    }
+  }
+}
