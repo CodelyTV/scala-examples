3386b1022504c3e6b613c33370185c70957018ee
Jordi Puig <jordi@xceed.me>
2018-02-03 16:42:09 +0100

    classes exercise

---
+++ src/main/tv/codely/scala_intro_examples/lesson_09_oop/exercise_solutions/jordi_puig/Email.scala
@@ -0,0 +1,31 @@
+package tv.codely.scala_intro_examples.lesson_09_oop.exercise_solutions.jordi_puig
+
+object Email {
+
+  val emailRegex = raw"""(\w+)@([\w\.]+)"""
+
+  def apply(email: String): Email = {
+    if (!validate(email)) throw new IllegalArgumentException("Invalid email format")
+
+    Email(Local.fromEmail(email),Domain.fromEmail(email))
+  }
+
+  def validate(email: String): Boolean = email.matches(emailRegex)
+}
+
+final case class Email(local: Local, domain: Domain) {
+  override def toString: String = s"${local.value}@${domain.value}"
+}
+
+object Local {
+  def fromEmail(email: String): Local = Local(email.substring(0, email.indexOf("@")))
+}
+
+
+final case class Local(value: String)
+
+object Domain {
+  def fromEmail(email: String): Domain = Domain(email.substring(email.indexOf("@") + 1))
+}
+
+final case class Domain(value: String)
---
+++ src/test/tv/codely/scala_intro_examples/lesson_09_oop/exercise_solutions/jordi_puig/EmailSpec.scala
@@ -0,0 +1,30 @@
+package tv.codely.scala_intro_examples.lesson_09_oop.exercise_solutions.jordi_puig
+
+import org.scalatest.{Matchers, WordSpec}
+
+class EmailSpec extends WordSpec with Matchers {
+  "Email" should {
+    "create an Email instance given a valid email" in {
+      val email = Email("staff@codely.tv")
+      email.toString shouldBe "staff@codely.tv"
+      email.local.value shouldBe "staff"
+      email.domain.value shouldBe "codely.tv"
+    }
+    "throw an IllegalArgumentException given an email without local" in {
+      assertThrows[IllegalArgumentException] {
+        Email("@codely.tv")
+      }
+    }
+    "throw an IllegalArgumentException given an email without domain" in {
+      assertThrows[IllegalArgumentException] {
+        Email("staff")
+      }
+    }
+    "throw an IllegalArgumentException given an email without @" in {
+      assertThrows[IllegalArgumentException] {
+        Email("staffcodely.tv")
+      }
+    }
+
+  }
+}
